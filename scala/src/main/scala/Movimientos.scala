b4fe8c1f61e6377c89c5ad461d8464fb4f25a7a9
fabfernandez <faba.jaja@gmail.com>
2018-11-04 20:26:31 -0300

    UsarItem ya compila

--- scala/src/main/scala/Movimientos.scala
+++ scala/src/main/scala/Movimientos.scala
@@ -4,6 +4,7 @@ import DragonBall.Guerrero
 
 object Movimiento {
 
+  class NoTieneItemException extends Exception
   type Contrincantes = (Guerrero, Guerrero)
   type PlanDeAtaque = List[Movimiento]
   type Movimiento = Contrincantes => Contrincantes
@@ -26,17 +27,19 @@ object cargarKi {
       if (atacante.tieneItem(item)) {
         (item, atacante, atacado) match {
           case (armaRoma, _ :Androide, _) => (atacante, atacado)
-          case (armaRoma, _, atacado :Biologico) => if(atacado.ki < 300) {(atacante, atacado.cambiarEstado(Inconsciente))}
-          case (armaFilosa, _, atacado :Sayajin) => if(atacado.tieneCola) {(atacante, atacado.perderCola.cambiarKi(1))}
+          case (armaRoma, _, atacado :Biologico) if atacado.ki < 300 =>(atacante, atacado.cambiarEstado(Inconsciente))
+          case (armaRoma, _, _) => (atacante, atacado)
+          case (armaFilosa, _, atacado :Sayajin) if atacado.tieneCola =>  (atacante, atacado.perderCola.cambiarKi(1))
           case (armaFilosa, _, atacado :Mono) => (atacante, atacado.getSayajin.perderCola.cambiarKi(1))
           case (armaFilosa, _, _ :Androide) => (atacante, atacado)
-          case (armaFilosa, atacante :Biologico, atacado :Biologico) => (atacante, atacado.cambiarKi(atacante.ki/100))
+          case (armaFilosa, atacante :Biologico, atacado :Biologico) => (atacante, atacado.cambiarKi(atacado.ki - atacante.ki/100))
           case (armaDeFuego, atacante :Biologico, atacado :Humano) => if(atacante.tieneMunicion) {(atacante.perderMunicion(), atacado.cambiarKi(atacado.ki - 20))}
                                       else (atacante, atacado)
           case (armaDeFuego, atacante, atacado :Namekusein) => if(atacante.tieneMunicion) {(atacante.perderMunicion(), atacado.cambiarKi(atacado.ki - 10))}
                                             else (atacante, atacado)
         }
       }
+      else throw new NoTieneItemException
     }
   }
 
