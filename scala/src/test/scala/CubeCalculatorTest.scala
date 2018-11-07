5e63636c131e143053551d364dd213fb074bd0f0
Lucia <lucia_roldan@outlook.com>
2018-11-07 14:08:14 -0300

    Nuestro test anda :)

--- scala/src/test/scala/CubeCalculatorTest.scala
+++ scala/src/test/scala/CubeCalculatorTest.scala
@@ -1,3 +1,5 @@
+import DragonBall.Movimiento.cargarKi
+import DragonBall.{Normal, Sayajin}
 import org.scalatest.FunSuite
 
 /*object CubeCalculator extends App {
@@ -7,7 +9,14 @@ import org.scalatest.FunSuite
 }*/
 
 class CubeCalculatorTest extends FunSuite {
-  test("CubeCalculator.cube") {
-    assert(27 === 27)
+  test("CubeCalculator.cube")
+    val goku = Sayajin(estado = Normal, ki = 100, nombre = "GOKU", inventario = List(), nivelSS = 1)
+
+    val vegeta = Sayajin(estado = Normal, ki = 100, nombre = "VEGETA", inventario = List(), nivelSS = 1)
+
+
+    val gokuKiCargado = cargarKi(goku, vegeta)._1
+
+    assert(gokuKiCargado.asInstanceOf[Sayajin].ki === 250)
   }
 }
