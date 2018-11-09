587caeec98cf24d11f933d832b35ea135e2ca5f4
fabfernandez <faba.jaja@gmail.com>
2018-11-09 15:44:59 -0300

    ahora si compila

--- scala/src/main/scala/Estado.scala
+++ scala/src/main/scala/Estado.scala
@@ -4,19 +4,19 @@ abstract class Estado(val roundsFajado: Int) {
  def dejarseFajar: Estado
 }
 
-case class Normal(roundsFajado: Int) extends Estado(roundsFajado: Int) {
+case class Normal(override val roundsFajado: Int) extends Estado(roundsFajado: Int) {
   override def dejarseFajar(): Estado ={
     this.copy(roundsFajado = roundsFajado + 1);
   }
 }
 
 
-case class Muerto(roundsFajado: Int) extends Estado(roundsFajado: Int){
+case class Muerto(override val roundsFajado: Int) extends Estado(roundsFajado: Int){
   override def dejarseFajar(): Estado ={
     this.copy(roundsFajado = 0)
   }
 }
-case class Inconsciente(roundsFajado: Int) extends Estado(roundsFajado: Int){
+case class Inconsciente(override val roundsFajado: Int) extends Estado(roundsFajado: Int){
   override def dejarseFajar(): Estado ={
     this.copy(roundsFajado = 0)
   }
